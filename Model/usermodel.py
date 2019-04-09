f55d4609483427ae46b1523bb9eddc9d0b120d6e
reezoobose <reezoobose@rediffmail.com>
2019-04-09 17:08:05 +0600

    bug fixed 2.0.2

--- Model/usermodel.py
+++ Model/usermodel.py
@@ -72,9 +72,12 @@ class UserModel(db.Model):
         :return: all users according to money .
         """
         # store all user .
-        all_user = cls.query.with_entities(cls.uuid).order_by(cls.us_dollar)
+        all_user = cls.query(cls.uuid).order_by(cls.us_dollar)
+        # print
+        for user in all_user:
+            print("All details --->"+user.uuid)
         # return the all user json format .
-        return {'Leader Board': "User Leader Board", 'User': [x.json( ) for x in all_user],
+        return {'Leader Board': "User Leader Board", 'Users': [x.json() for x in all_user],
                 "Success_Code": 1}, 200
 
     # </editor-fold>
