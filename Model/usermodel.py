24c53488a093877df5271aee2f4debe678a3ae7f
reezoobose <reezoobose@rediffmail.com>
2019-05-26 17:59:28 +0600

    School Leader Board Modified

--- Model/usermodel.py
+++ Model/usermodel.py
@@ -107,8 +107,6 @@ class UserModel(db.Model):
         # iterate to createa dictionary .
         for num,item in enumerate(school_id_list, start=0):
             resultDict[item] = school_obtained_coin_list[int(num)]
-            print("Key---> {0} Value----->{1}".format(resultDict[item],school_obtained_coin_list[int(num)])
-
         #Sort the dictionary.
         sorted_x = sorted(resultDict.items( ), key=lambda x: x[1])
         # make it json serialize
